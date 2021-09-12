a7e553ea3a4ef140ced26dc2501edf7144414a40
petro.barnabas <petro.barnabas@njit.hu>
2021-09-12 14:26:11 +0200

    Create nevek.php

---
+++ nevek.php
@@ -0,0 +1,9 @@
+<?php
+    $t = ["Robert Downey Jr.", "Chris Hemsworth", "Scarlett Johansson", "Karen Gillan", "Benedict Cumberbatch"];
+
+    foreach($t as $nev)
+    {
+        echo "- $nev\n";
+    }
+
+?>
